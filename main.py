d3784a2b34a65773ce24317d3bec9b0acd970ecd
Daniil4949 <91974808+Daniil4949@users.noreply.github.com>
2022-01-24 11:11:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ def person_is_seller(name):
     return name[-1]=='m'
 from collections import deque
 search_queue=deque()
-search_queue+=graph['you']
-while search_queue:
-    person=search_queue.popleft()
-    if person_is_seller(person):
-        print(person,'is a mango seller!')
-    else:
-        search_queue+=graph[person]
+def search(name):
+    search_queue=deque()
+    search_queue+=graph[name]
+    searched=[]
+    while search_queue:
+        person=search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                      print(person,'is a mango seller!')
+            else:
+                search_queue+=graph[person]
+                searched.append(person)
+search('you')
 
 
 
